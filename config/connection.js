dcc1dc042e3d38bc3cbac8ad1d3ba1ec8303917a
AC <alexandracampau@gmail.com>
2021-12-17 11:33:26 -0700

    git ignore

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_PW,
   {
     host: "localhost",
     dialect: "mysql",
