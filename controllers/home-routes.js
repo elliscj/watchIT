65f51d2d2ae034759edfe5fda611c17c2425b892
Cam Ellis <elliscj@colorado.edu>
2021-12-29 14:00:28 -0700

    fixed ref links in handlebars/troubleshooting server and handlebar rendering

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,4 +4,16 @@ const router = require("express").Router();
 // const { User } = require("../models");
 // const withAuth = require("../utils/auth");
 
+router.get("/", async (req, res) => {
+  try {
+    res.render("index", {
+      galleries,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
