8a45a8bae09043b2e65645fc5cc12fc15eeb43e1
Cam Ellis <elliscj@colorado.edu>
2021-12-31 13:00:35 -0700

    added hooks to user model, changes to login-form.js

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -15,6 +15,26 @@ router.get("/", async (req, res) => {
   }
 });
 
+// create new user
+// router.post("/", async (req, res) => {
+//   try {
+//     const dbUserData = await User.create({
+//       username: req.body.username,
+//       email: req.body.email,
+//       password: req.body.password,
+//     });
+
+//     req.session.save(() => {
+//       req.session.loggedIn = true;
+
+//       res.status(200).json(dbUserData);
+//     });
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json(err);
+//   }
+// });
+
 // get all users (qty) (for admin purposes?)
 
 // verify login? get password where Users.email = _____.
